2f92958951e82b49f46f4289d0ce45c6b041979d
Victor <VictorCarmona9035@hotmail.com>
2023-10-03 18:49:45 +0200

    3-Mostrando en el carrito el curso seleccionado

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
 
 cargarEventListeners();
 //Registrar todos los eventos
@@ -27,7 +28,7 @@ function agregarCurso(e) {
 
 //Lee el contenido del html al que le dimos click y extare la informacion del curso
 function leerDatosCurso(curso) {
-  console.log(curso);
+  //console.log(curso);
 
   //Crear un objeto con el contenido del curso
   const infoCurso = {
@@ -35,8 +36,42 @@ function leerDatosCurso(curso) {
     titulo: curso.querySelector("h4").textContent,
     precio: curso.querySelector(".precio span").textContent,
     // precioAnterior: curso.querySelector("p").textContent,
-    id: curso.querySelector('a').getAttribute('data-id'),
-    cantidad: 1
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
   };
-  console.table(infoCurso)
+
+  //Agrega elementos al arreglo del carrito
+  articulosCarrito = [...articulosCarrito, infoCurso];
+  carritoHTML();
+
+  console.log(articulosCarrito);
+}
+
+//Muestra el carrito de compras  en el HTML
+function carritoHTML() {
+  //Limpiar el HTML
+  limpiarHTML();
+
+  //Recorre el carrito y genera el HTML
+  articulosCarrito.forEach((curso) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>
+        ${curso.titulo}
+        </td>`;
+
+    //Agrega el HTML del carrito en el body
+    contenedorCarrito.appendChild(row);
+  });
+}
+
+//Elimina los cursos de tbody
+function limpiarHTML() {
+  //Forma lenta
+  contenedorCarrito.innerHTML = "";
+
+  //Forma rapida, MIentras haya un hijo que la condicion se cumpla
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+  }
 }
